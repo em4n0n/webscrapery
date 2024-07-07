c16549cacaf8708275e23e40fcd56212cea5a266
em4n0n <58372945+em4n0n@users.noreply.github.com>
2024-07-06 18:58:21 -0700

    Update main.py

--- main.py
+++ main.py
@@ -2,17 +2,17 @@ from bs4 import BeautifulSoup
 import requests
 
 search = input("Search for:")
-params = {"q": search} # search variable q to what we wan to search for
+params = {"q": search} # search variable q to what we want to search for
 
 r = requests.get("https://www.bing.com/search", params=params)
 
 soup = BeautifulSoup(r.text, "html.parser")
-results = soup.find("ol", {"id": "b_results"})
-links = results.findAll("Li", {"class": "b_algo"})
+results = soup.find("ol", {"id": "b_results"}) # first instance of an ordered list and the attributes to look for
+links = results.findAll("Li", {"class": "b_algo"}) # find all list items with a class b_algo to store them in a list called links
 
 for item in links:
     item_text = item.find("a").text
-    item_href = item.find("a").attrs("href")
+    item_href = item.find("a").attrs("href") # find the attribute href
 
     if item_text and item_href:
         print(item_text)
