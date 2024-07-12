8d86f2ac7f77f4f6fc8fb7b42c52c59d671b84cd
em4n0n <58372945+em4n0n@users.noreply.github.com>
2024-07-11 21:05:32 -0700

    image with BytesIO

--- images.py
+++ images.py
@@ -13,3 +13,4 @@ links = soup.findAll("a", {"class": "thumb"})
 for item in links:
     image_obj = requests.get(item.attrs["href"])
     title = item.attrs["href"].split("/")[-1]
+    img = Image.open(BytesIO(image_obj).content)
