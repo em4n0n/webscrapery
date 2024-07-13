b6123ca89ad9ad2167741c76935ba98fae3434e4
em4n0n <58372945+em4n0n@users.noreply.github.com>
2024-07-12 21:38:27 -0700

    Update images.py

--- images.py
+++ images.py
@@ -11,7 +11,7 @@ soup = BeautifulSoup(r.text, "html.parser")
 links = soup.findAll("a", {"class": "thumb"})
 
 for item in links:
-    image_obj = requests.get(item.attrs["href"])
+    img_obj = requests.get(item.attrs["href"])
     title = item.attrs["href"].split("/")[-1]
-    img = Image.open(BytesIO(image_obj.content))
-    img.save("./scraped_images" + title, img.format)
+    img = Image.open(BytesIO(img_obj.content))
+    img.save("./scraped_images/" + title, img.format)
