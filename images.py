779ab6e144113e4b720d37b2202ab9a82826db40
em4n0n <58372945+em4n0n@users.noreply.github.com>
2024-07-13 22:01:47 -0700

    Add print statement of href

--- images.py
+++ images.py
@@ -4,7 +4,6 @@ from PIL import Image
 from io import BytesIO
 
 def StartSearch():
-
     search = input("Search for:")
     params = {"q": search}
     r = requests.get("http://www.bing.com/images/search", params=params)
@@ -14,6 +13,7 @@ def StartSearch():
 
     for item in links:
         img_obj = requests.get(item.attrs["href"])
+        print("Getting", item.attrs["href"])
         title = item.attrs["href"].split("/")[-1]
         img = Image.open(BytesIO(img_obj.content))
         img.save("./scraped_images/" + title, img.format)
