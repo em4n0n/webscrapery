a8abedcbb57b93613ff08474efd1ff8e2fcd5f94
em4n0n <58372945+em4n0n@users.noreply.github.com>
2024-07-13 22:22:05 -0700

    Try and except statement

--- images.py
+++ images.py
@@ -18,14 +18,17 @@ def StartSearch():
     links = soup.findAll("a", {"class": "thumb"})
 
     for item in links:
-        img_obj = requests.get(item.attrs["href"])
-        print("Getting", item.attrs["href"])
-        title = item.attrs["href"].split("/")[-1]
         try:
-            img = Image.open(BytesIO(img_obj.content))
-            img.save("./" + dir_name+ title, img.format)
+            img_obj = requests.get(item.attrs["href"])
+            print("Getting", item.attrs["href"])
+            title = item.attrs["href"].split("/")[-1]
+            try:
+                img = Image.open(BytesIO(img_obj.content))
+                img.save("./" + dir_name+ title, img.format)
+            except:
+                print("Could not save image.")
         except:
-            print("Could not save image.")
+            print("Could not request image")
 
     StartSearch()
 
